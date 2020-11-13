08ebc6a8ca7c56aa9aa7592ed64d4de834c074a7
Luan Marreiros Silva <lmarreir@everis.com>
2020-11-13 16:45:45 -0300

    melhoria do editar

--- src/app/components/edit/edit.component.ts
+++ src/app/components/edit/edit.component.ts
@@ -15,15 +15,26 @@ export class EditComponent implements OnInit {
   category;
   categoriesList = [];
 
+  nome_tarefa;
+  nome_categoria;
+  descricao;
+  data_inicio;
+  data_final;
+  selected;
+
   constructor(
     private dialogRef: MatDialogRef<EditComponent>,
     @Inject(MAT_DIALOG_DATA) data,
     private taskControl: TaskControlService) {
-      this.category = data;
-    }
+    this.nome_tarefa = data.nome_tarefa;
+    this.nome_categoria = data.nome_categoria;
+  }
 
   ngOnInit(): void {
     this.pegarCategorias();
+
+    console.log(this.category)
+
   }
 
   pegarCategorias() {
@@ -31,23 +42,23 @@ export class EditComponent implements OnInit {
 
       this.category = data;
 
-      try{
+      try {
         for (let i = 0; i < 300; i++) {
           if (this.categoriesList.includes(this.category[i].nome_categoria) != true) {
             this.categoriesList.push(this.category[i].nome_categoria)
           }
         }
-      }catch{
+      } catch {
       }
 
     });
   }
 
-  fechar() { 
+  fechar() {
     this.dialogRef.close()
   }
 
-  salvar() { 
+  salvar() {
     console.log(this.category);
     this.fechar();
   }
